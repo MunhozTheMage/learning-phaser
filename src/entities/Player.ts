48f39f944edbd40a2091e46b5190ad5c14d862e9
Gabriel Munhoz Nóbrega <gmnobrega0704@gmail.com>
2021-06-03 11:54:55 -0300

    Grid snap movement

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -3,13 +3,37 @@ import BaseEntity from "./BaseEntity";
 import { someKeyDown } from "../utils";
 
 export default class extends BaseEntity {
+  state: { movement: { moving: boolean, progress: number, axis: "x" | "y" | null, direction: 1 | -1 | 0 } }
+
   constructor({ game, x, y, asset } : { game: any, x: number, y: number, asset: string }) {
     super({ game, x, y, asset })
 
     this.sprite.anchor.setTo(0.5)
+
+    this.state = {
+      movement: {
+        moving: false,
+        progress: 0,
+        axis: null,
+        direction: 0,
+      }
+    }
   }
 
-  update() {
+  resetMovementState() {
+    this.state.movement.axis = null;
+    this.state.movement.direction = 0;
+    this.state.movement.moving = false;
+    this.state.movement.progress = 0
+  }
+
+  setMovingOn(axis: "x" | "y", direction: 1 | -1) {
+    this.state.movement.axis = axis;
+    this.state.movement.direction = direction;
+    this.state.movement.moving = true;
+  }
+
+  fluidMoveDetector() {
     if (someKeyDown(this.game, [Phaser.Keyboard.LEFT, Phaser.Keyboard.A])) {
       this.transformX(
         this.sprite.x - 5 >= 0 ? -5 : -(this.sprite.x)
@@ -31,6 +55,37 @@ export default class extends BaseEntity {
         this.sprite.y + 5 <= this.game.world.height ? 5 : this.game.world.height - this.sprite.y
       )
     }
+  }
+
+  snapMoveDetector() {
+    const speed = 1;
+    const tileSize = 10;
+
+    if(!this.state.movement.moving) {
+      if (someKeyDown(this.game, [Phaser.Keyboard.LEFT, Phaser.Keyboard.A])) {
+        this.setMovingOn("x", -1);
+      }
+      else if (someKeyDown(this.game, [Phaser.Keyboard.RIGHT, Phaser.Keyboard.D])) {
+        this.setMovingOn("x", 1);
+      }
+      else if (someKeyDown(this.game, [Phaser.Keyboard.UP, Phaser.Keyboard.W])) {
+        this.setMovingOn("y", -1);
+      }
+      else if (someKeyDown(this.game, [Phaser.Keyboard.DOWN, Phaser.Keyboard.S])) {
+        this.setMovingOn("y", 1);
+      }
+    } else {
+      this.state.movement.progress + speed <= tileSize ?
+        (this.state.movement.progress += speed) && 
+        (this.sprite[this.state.movement.axis] += this.state.movement.progress * this.state.movement.direction) :
+        (this.sprite[this.state.movement.axis] += (tileSize - this.state.movement.progress) * this.state.movement.direction) &&
+        this.resetMovementState()
+    }
+  }
+
+  update() {
+    // this.fluidMoveDetector()
+    this.snapMoveDetector()
 
     if(this.game.input.mouse.wheelDelta !== 0) {
       this.sprite.angle += this.game.input.mouse.wheelDelta * 5;
